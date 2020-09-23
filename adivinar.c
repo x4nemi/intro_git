9f38d8b4fd3b03a1b0145c37bd02debe54a1d3ef
Ximena Vega <ximena.garcia7482@alumnos.udg.mx>
2020-09-23 14:01:52 -0500

    Cambie las instrucciones para el usuario porque estaban confusas

--- adivinar.c
+++ adivinar.c
@@ -6,26 +6,24 @@
 #define FIN 100
 
 int adivinar(int ini, int fin); //Prototipo de la función de forma entera, que requiere dos enteros
-
-
  
 int main(){ //Función principal
-	int adi=adivinar(INI, FIN); //Determinamos la variable que utilizaremos
+	int x = adivinar(INI, FIN); //Determinamos la variable que utilizaremos
 	
-	printf("El numero fue encontrado en %d intentos", adi);
+	printf("El numero fue encontrado en %d intentos", x);
 }
 
 int adivinar(int ini, int fin){ //Inicio de la función "adivinar"
 	int i = 0, rango = 0; 
 	bool ad = false; 
-    char r = ''; 
+    char r = 'x'; 
 	
 	printf("Adivinare un numero que estes pensando entre %d y %d en maximo %d intentos\n", INI, FIN, (int)log2(FIN-INI+1)+1); //Mandamos a pantalla el objetivo del programa
 	getchar(); //Esperamos por una tecla
 	
 	rango=(INI + FIN) / 2;
 	do{ //Ciclo de instrucciones, mientras que "ad" sea "FALSE"
-		printf("Es mayor, menor o igual a %d?\n", rango);
+		printf("Es mayor, menor o igual a %d (> / < / =)?\n", rango);
 		scanf("%s", &r);
 				
 		
